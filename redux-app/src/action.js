ae61b3743b86d84ed1fb4d6a7599c4f18e7440fe
raguram123 <jithucrazoo1@gmail.com>
2023-12-14 18:10:48 +0530

    redux-app

---
+++ redux-app/src/action.js
@@ -0,0 +1,18 @@
+
+export const ADD_TO_CART='ADD_TO_CART';
+export const REMOVE_FROM_CART='REMOVE_FROM_CART';
+export const UPDATE_QUANTITY='UPDATE_QUANTITY';
+
+export const addToCart=(product)=>({
+  type:ADD_TO_CART,
+  payload:product,
+});
+export const removeFromCart=productId=>({
+  type:REMOVE_FROM_CART,
+  payload:{id:productId}  
+});
+
+export const updateQuantity=(productId,newQuantity)=>({
+  type:UPDATE_QUANTITY,
+  payload:{productId,newQuantity}
+});
